5f1f1e6a4568c320c2da43199298157db26eb5f9
Yasmim <yasmimferro06@gmail.com>
2023-06-20 10:02:37 -0300

    Att

--- src/main/java/com/itb/tcc/cafeteria/controller/LojaController.java
+++ src/main/java/com/itb/tcc/cafeteria/controller/LojaController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -17,7 +18,7 @@ public class LojaController {
 	List<Produto> listaDeProdutos = new ArrayList<Produto>();
 
 	@GetMapping ("/listar")
-	public String listarprodutos() {
+	public String listarprodutos(Model model) {
 		
 		Produto p1 = new Produto();
 		p1.setId(20l);
@@ -37,6 +38,9 @@ public class LojaController {
 		
 		listaDeProdutos.add(p1);
 		listaDeProdutos.add(p2);
+		
+		model.addAttribute("listaDeProdutos", listaDeProdutos);
+		
 		return "produtos";
 	}
 }
